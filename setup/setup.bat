414fc980d61092a278ffa601598989d66f82a3bb
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-01-12 08:20:42 +0800

    fix setup/setup.bat script

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c43fd80582f5496a88a62c9ea929ed1af043d550e6ba3068b16e12b4ba27d1b6
-size 1423
+oid sha256:af15753aa5bd04a60a65811d4800e57743dfb0d50e383d2018e956cd8dd457d2
+size 1426
