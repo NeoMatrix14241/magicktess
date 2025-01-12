6fa0e3e7eda2273fb82990a138df268a7c727784
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-01-12 08:10:35 +0800

    added hyperthreading support (experimental and might not work yet)

--- start_process.bat
+++ start_process.bat
@@ -40,19 +40,19 @@ if "%scriptPath:~-1%"=="\" set "scriptPath=%scriptPath:~0,-1%"
 
 where pwsh >nul 2>nul
 if %errorlevel% neq 0 (
-    powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktess-st.ps1', 'magicktess-mt.ps1', 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
+    powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktesstk-st.ps1', 'magicktesstk-mt.ps1', 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
     powershell.exe -Command "Get-ChildItem -Path '%scriptPath%'\setup' -Recurse -File | Unblock-File"
-    powershell.exe -ExecutionPolicy RemoteSigned -File "magicktess-st.ps1" "input"
+    powershell.exe -ExecutionPolicy RemoteSigned -File "magicktesstk-st.ps1" "input"
 ) else (
     pwsh -NoProfile -Command "exit" >nul 2>nul
     if %errorlevel% neq 0 (
-        powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktess-st.ps1', 'magicktess-mt.ps1' 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
+        powershell.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktesstk-st.ps1', 'magicktesstkk-mt.ps1' 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
         powershell.exe -Command "Get-ChildItem -Path '%scriptPath%\setup' -Recurse -File | Unblock-File"
-        powershell.exe -ExecutionPolicy RemoteSigned -File "magicktess-st.ps1" "input"
+        powershell.exe -ExecutionPolicy RemoteSigned -File "magicktesstk-st.ps1" "input"
     ) else (
-        pwsh.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktess-st.ps1', 'magicktess-mt.ps1' 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
+        pwsh.exe -Command "Get-ChildItem -Path '%scriptPath%' -File | Where-Object { $_.Name -in @('magicktesstk-st.ps1', 'magicktesstk-mt.ps1' 'start_process.bat', 'ReadMe.txt') } | Unblock-File"
         pwsh.exe -Command "Get-ChildItem -Path '%scriptPath%'\setup' -Recurse -File | Unblock-File"
-        pwsh.exe -ExecutionPolicy RemoteSigned -File "magicktess-mt.ps1" "input"
+        pwsh.exe -ExecutionPolicy RemoteSigned -File "magicktesstk-mt.ps1" "input"
     )
 )
 
